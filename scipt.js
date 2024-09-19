e10036938dbc976a9cda69558ead712c8bb6ac93
Kayshow21 <tiannakayshow@gmail.com>
2024-09-19 12:38:12 +0100

    changes

---
+++ scipt.js
@@ -0,0 +1,140 @@
+//  javascript is a programming language 
+
+// javascirpt is a high level programming language
+
+// javascript is a object oriented high level programming language
+
+// javascipt is an object oriented multi paradigm high level programming language
+
+// VALUES AND VARIABLES;
+// Data types
+// 1;Numbers
+// 2:String
+// 3:Booleane
+
+// 4:Undefined
+// 5: Null 
+
+// Comparison operators
+// 1:Greater than >
+// 2:Less than <
+// 3;Greater or equals >=
+// 4: less or equal <=
+// 5: loosely equal<=
+// 6:Strictly equal:===
+// 7:loosly not !=
+// 8:Strictly noy equal !==
+
+// Logical operators
+// 1:AND &&
+// 2: OR ||
+// 3: NOT !
+
+// Strings are itirables they are wriiten in code
+
+// LOOP
+// for loop is the most important indentation and incrementation
+ 
+// let  fName = "Kanyinsola";
+
+// let lName = "Sulola";
+
+// let fullName = "My name is " + lName  +  " " + fName;
+
+// console.log(fullName);
+
+
+// let firstName = "Kanyinsola";
+
+// let fulllName = "The dog chased " + firstName  +  " to the Sulola's` house ";
+
+// // Template literals
+
+// let newfulllName = `The dog chased  ${firstName}  to the Sulola's  house`;
+
+// console.log(fulllName);
+// console.log(newfulllName);
+
+//  for (let i = 0; i < 6; i++) { 
+//    console.log(fName [i])
+//  }
+// console.log("inverse");
+
+//  for (let i = 5; i >= 0; i--) {
+//     console.log(fName[i])
+//  }
+// let age = 16;
+// // console.log(x);
+// if (age == 18)  {
+//     console.log( "congrats you are an adult")
+// }else{
+//  console.log("please! contact your parents and send feedbacks THANK YOU!")
+// }
+
+// ARRAY
+// propery and method
+// let room = 2023;
+// let num = [1, 2, "Mike", 4, room];
+
+// console.log(num[num.length -3])
+// Aarrays propety
+// num.push("Nife")  
+// num.pop();
+// num.shift();
+// num.unshift("Nife");
+ 
+
+// for(let i = 0; i< num.length; i++ ) {
+//     console.log(num[i])
+// }
+
+ 
+//  Arrays property
+
+// console.log(num.length);
+
+// function declaration
+// function display() {
+//   console.log("Welcome to Javascript functions");
+// }
+
+// function call
+// display();
+
+// functions with paprameteres
+
+// function calcAgeNew(newbirthYear) {
+//   console.log(2024 - newbirthYear);
+// }
+
+// Calling functiion with argumments 
+
+// calcAgeNew(1990);
+// calcAgeNew(1997);
+// calcAgeNew(1999);
+// calcAgeNew(2000);
+// calcAgeNew(2021);
+
+
+
+// // objects
+
+// let fName =  ["Kanyinsola", "Sulola", 15, ["Nifemi"  "Micheal"] ]
+
+
+// let Kshowobject = {
+//   firstName: "Kanyinsola",
+//   lastName: "Sulola",
+//   age: 16,
+//   pet: "Dog",
+//   friends:["Nifemi", "Micheal"],
+  
+// };
+
+// console.log(Kshowobject);
+
+// DOM manipulation
+
+let h1 = document.querySelector("h1");
+
+// h1.textContent = "This is a Web Developers Page"
